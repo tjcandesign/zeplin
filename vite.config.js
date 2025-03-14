cb707de84b31c1cfdaf7c178377a150fce7fa494
Tj Cichecki <tj@wrkhrs.co>
2025-03-14 11:57:52 -0400

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/zeplin/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
