06d7bac64bb10fa250111531ee8701b41c46cfc5
Tj Cichecki <tj@wrkhrs.co>
2025-03-14 12:17:08 -0400

    Update Vite config for proper asset handling

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,14 +8,22 @@ export default defineConfig({
   base: '/zeplin/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
+    sourcemap: true,
+    assetsDir: '',
     rollupOptions: {
+      input: {
+        app: resolve(__dirname, 'index.html')
+      },
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
     }
   }
 })
